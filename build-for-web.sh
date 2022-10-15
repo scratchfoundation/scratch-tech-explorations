395d3467f0c8f79f2df731ea6c196a7cbf60fb7d
Christopher Willis-Ford <7019101+cwillisf@users.noreply.github.com>
2022-10-14 17:38:49 -0700

    feat: build for web

---
+++ build-for-web.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+set -e
+
+# one-time setup:
+# rustup target add wasm32-unknown-unknown
+# cargo install -f wasm-bindgen-cli
+
+# the "--no-default-features" setting is here to disable dynamic linking, which isn't supported for wasm
+# see the [features] section of Cargo.toml for more detail
+
+for PROFILE in web-dev web-release; do
+    cargo build --profile $PROFILE --no-default-features --target wasm32-unknown-unknown
+    wasm-bindgen --target web --out-dir ./target/web/$PROFILE ./target/wasm32-unknown-unknown/$PROFILE/scratch-bevy.wasm
+done
