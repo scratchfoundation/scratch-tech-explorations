780dba67c017cac07ab848e9dd6a0e9dff1fab0f
Christopher Willis-Ford <7019101+cwillisf@users.noreply.github.com>
2022-12-09 20:20:27 -0800

    feat: load a project file and display a loading screen

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,19 @@
 use bevy::{
     prelude::*,
+    tasks::*,
     time::FixedTimestep,
     window::*,
 };
+use futures_lite::future;
 
 const TIME_STEP: f64 = 1. / 30.;
+
+#[derive(Debug, Clone, Eq, PartialEq, Hash)]
+
+enum AppState {
+    Loading,
+    Running,
+}
 
 fn main() {
     App::new()
@@ -20,10 +29,67 @@ fn main() {
             },
             ..default()
         }))
+        .add_plugin(ScratchLoadingScreenPlugin)
         .add_plugin(ScratchStagePlugin)
         .add_plugin(ScratchDemoProjectPlugin)
         .add_system(close_on_esc)
         .run();
+}
+
+//
+// Loading screen
+//
+
+pub struct ScratchLoadingScreenPlugin;
+
+#[derive(Component)]
+struct LoadingScreen;
+
+impl Plugin for ScratchLoadingScreenPlugin {
+    fn build(&self, app: &mut App) {
+        app
+            .add_state(AppState::Loading)
+            .add_system_set(
+                SystemSet::on_enter(AppState::Loading)
+                    .with_system(ScratchLoadingScreenPlugin::start_loading_screen)
+            )
+            .add_system_set(
+                SystemSet::on_update(AppState::Loading)
+                    .with_system(ScratchLoadingScreenPlugin::update_loading_screen)
+            )
+            .add_system_set(
+                SystemSet::on_exit(AppState::Loading)
+                    .with_system(ScratchLoadingScreenPlugin::stop_loading_screen)
+            );
+    }
+}
+
+impl ScratchLoadingScreenPlugin {
+
+    fn start_loading_screen(mut commands: Commands, asset_server: Res<AssetServer>) {
+        commands.spawn((
+            SpriteBundle {
+                texture: asset_server.load("squirrel.png"),
+                ..default()
+            },
+            LoadingScreen
+        ));
+    }
+
+    fn update_loading_screen(mut loading_screen_query: Query<&mut Transform, With<LoadingScreen>>) {
+        let one_degree_in_radians: f32 = (1.0_f32).to_radians();
+
+        for mut transform in &mut loading_screen_query {
+            transform.rotate_z(-one_degree_in_radians);
+        }
+    }
+
+    fn stop_loading_screen(mut commands: Commands, mut loading_screen_query: Query<Entity, With<LoadingScreen>>) {
+        for loading_screen in &mut loading_screen_query {
+            commands.entity(loading_screen).despawn();
+        }
+
+    }
 }
 
 //
@@ -109,31 +175,46 @@ pub struct ScratchDemoProjectPlugin;
 impl Plugin for ScratchDemoProjectPlugin {
     fn build(&self, app: &mut App) {
         app
-            .add_startup_system(add_demo_project_sprites);
+            .add_startup_system(project_load)
+            .add_system_set(
+                SystemSet::on_update(AppState::Loading)
+                    .with_system(project_check_load)
+            );
     }
 }
 
-fn add_demo_project_sprites(mut commands: Commands, asset_server: Res<AssetServer>) {
-    commands.spawn(
-        SpriteBundle {
-            texture: asset_server.load("squirrel.png"),
-            transform: Transform::from_scale(Vec3::new(0.5, 0.5, 1.)),
-            ..default()
-        })
-        .insert(Name("Sprite 1".to_string()))
-        .insert(ScratchScripts(vec![
-            ScratchCode::MoveOneStep,
-            ScratchCode::TurnClockwise,
-        ]));
-    commands.spawn(
-        SpriteBundle {
-            texture: asset_server.load("squirrel.png"),
-            transform: Transform::from_scale(Vec3::new(0.5, 0.5, 1.)),
-            ..default()
-        })
-        .insert(Name("Sprite 2".to_string()))
-        .insert(ScratchScripts(vec![
-            ScratchCode::MoveTwoSteps,
-            ScratchCode::TurnCounterClockwise,
-        ]));
+#[derive(Resource)]
+struct ProjectZip(HandleUntyped);
+
+#[derive(Resource)]
+struct ProjectLoadTask(Task<i32>);
+
+fn project_load(mut commands: Commands, asset_server: Res<AssetServer>) {
+    info!("Starting project load");
+    let project_handle = asset_server.load_untyped("Infinite Toebeans.sb3");
+    commands.insert_resource(ProjectZip(project_handle))
+}
+
+fn project_check_load(mut commands: Commands, mut app_state: ResMut<State<AppState>>, project_zip: Option<Res<ProjectZip>>, mut project_task: Option<ResMut<ProjectLoadTask>>) {
+    if let Some(project_zip) = project_zip {
+        if project_zip.is_added() {
+            info!("Project archive is loaded. Time to unpack...");
+            let thread_pool = AsyncComputeTaskPool::get();
+            let load_task = thread_pool.spawn(async move {
+                let start_time = std::time::Instant::now();
+                while start_time.elapsed() < std::time::Duration::from_secs_f32(4.2)
+                {
+                    // spin
+                }
+                42 // return hydrated project
+            });
+            commands.insert_resource(ProjectLoadTask(load_task));
+        }
+        else if let Some(project_task) = &mut project_task {
+            if let Some(project_data) = future::block_on(future::poll_once(&mut project_task.0)) {
+                info!("Project data is: {}", project_data);
+                app_state.set(AppState::Running).unwrap();
+            }
+        }
+    }
 }
