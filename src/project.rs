a0606ac3110364f8c0afd333572555e352e923e5
Christopher Willis-Ford <7019101+cwillisf@users.noreply.github.com>
2023-01-27 16:05:20 -0800

    feat: extract and parse project JSON

--- src/project.rs
+++ src/project.rs
@@ -4,7 +4,7 @@ use bevy::{
 };
 use futures_lite::future;
 use std::{fs, path::Path};
-use zip::ZipArchive;
+use zip::{ZipArchive, result::ZipError};
 
 use crate::AppState;
 
@@ -21,39 +21,101 @@ impl Plugin for ScratchDemoProjectPlugin {
     }
 }
 
+type ProjectLoadResult = Result<serde_json::Value, ProjectLoadError>;
+
+#[derive(Debug)]
+enum ProjectLoadError {
+    IoError(std::io::Error),
+    ParseError(serde_json::Error),
+    ZipError(ZipError),
+}
+
+impl From<std::io::Error> for ProjectLoadError {
+    fn from(err: std::io::Error) -> Self {
+        Self::IoError(err)
+    }
+}
+
+impl From<serde_json::Error> for ProjectLoadError {
+    fn from(err: serde_json::Error) -> Self {
+        Self::ParseError(err)
+    }
+}
+
+impl From<ZipError> for ProjectLoadError {
+    fn from(err: ZipError) -> Self {
+        Self::ZipError(err)
+    }
+}
+
+impl std::fmt::Display for ProjectLoadError {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        match self {
+            Self::IoError(err) => write!(f, "{}", err),
+            Self::ParseError(err) => write!(f, "{}", err),
+            Self::ZipError(err) => write!(f, "{}", err),
+        }
+    }
+}
+
 #[derive(Resource)]
-struct ProjectLoadTask(Task<usize>);
+struct ProjectLoadTask(Task<ProjectLoadResult>);
 
 fn project_load(mut commands: Commands) {
     info!("Starting project load");
     let thread_pool = AsyncComputeTaskPool::get();
     let load_task = thread_pool.spawn(async move {
-        deserialize_sb3("assets/Infinite ToeBeans.sb3").await
+        load_sb3("assets/Infinite ToeBeans.sb3").await
     });
     commands.insert_resource(ProjectLoadTask(load_task));
 }
 
 fn project_check_load(mut app_state: ResMut<State<AppState>>, mut project_task: Option<ResMut<ProjectLoadTask>>) {
     if let Some(project_task) = &mut project_task {
-        if let Some(project_data) = future::block_on(future::poll_once(&mut project_task.0)) {
-            info!("Project data is: {}", project_data);
+        if let Some(project_load_result) = future::block_on(future::poll_once(&mut project_task.0)) {
+            match project_load_result {
+                Ok(project_data) => info!("Project loaded data: {}", project_data),
+                Err(project_error) => error!("Project load failure: {}", project_error),
+            }
+
             app_state.set(AppState::Running).unwrap();
         }
     }
 }
 
-async fn deserialize_sb3(path: impl AsRef<Path>) -> usize {
+async fn load_sb3(path: impl AsRef<Path>) -> ProjectLoadResult {
+    let project_content = deserialize_sb3(path).await;
+
+    // validate project content
+    // stop the VM
+    // install project into runtime
+    // start the VM
+
+    // artificial delay
+    {
+        info!("delaying for a bit so you can watch the pretty loading screen");
+        let start_time = std::time::Instant::now();
+        while start_time.elapsed() < std::time::Duration::from_secs_f32(4.2)
+        {
+            // spin to pretend we're loading lots of stuff
+        }
+    }
+
+    project_content
+}
+
+async fn deserialize_sb3(path: impl AsRef<Path>) -> ProjectLoadResult {
 
     // TODO: would it make sense to use async_zip instead? ...but will tokio conflict with bevy?
 
-    let file = fs::File::open(&path).unwrap();
-    let sb3_zip = ZipArchive::new(file).unwrap();
+    let file = fs::File::open(&path)?;
 
-    let start_time = std::time::Instant::now();
-    while start_time.elapsed() < std::time::Duration::from_secs_f32(4.2)
-    {
-        // spin to pretend we're loading lots of stuff
-    }
+    // this will open the ZIP and read the central directory
+    let mut sb3_zip = ZipArchive::new(file)?;
 
-    sb3_zip.len() // return hydrated project
+    let project_json_reader = sb3_zip.by_name("project.json")?;
+
+    let project_json: serde_json::Value = serde_json::from_reader(project_json_reader)?;
+
+    Ok(project_json) // return hydrated project
 }
